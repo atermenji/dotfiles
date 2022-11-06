06d33c74db6d86b15c2c7ed8d71fd0100a75d2bb
Artur Termenji <atermenji@gmail.com>
2022-11-06 23:52:18 +0200

    sh list of install

---
+++ .config/installs.sh
@@ -0,0 +1,9 @@
+# https://github.com/junegunn/vim-plug#installation
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# https://github.com/oh-my-fish/oh-my-fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+# https://github.com/powerline/fonts
+# run install.sh
